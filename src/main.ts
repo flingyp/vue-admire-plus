e0e28432d9d372fdea7f2146fc58d1aea2602f83
flingyp <1213563369@qq.com>
2022-05-30 17:37:44 +0800

    feat: 集成ElementPlus、配置组件的自动导入，添加浏览器默认样式重置文件

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 浏览器默认样式重置文件
+import '@/style/ResetStyle.css'
+
+/**
+ * 初始化应用
+ */
+const initApplication = async () => {
+  const app = createApp(App)
+
+  // ElementPlus
+  app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+
+  app.mount('#app')
+}
+
+await initApplication()
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
+import type { UserConfigExport } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D
+import { resolve } from 'path'
+
+export default (config: UserConfigExport): UserConfigExport => {
+  return {
+    base: './',
+    // 配置别名
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
+}
