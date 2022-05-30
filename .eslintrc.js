c811e3f0a26f1251f751c6cc1af32ba9d815e9fd
flingyp <1213563369@qq.com>
2022-05-30 13:32:53 +0800

    feat: 集成代码规范的插件

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'vue/setup-compiler-macros': true // 处理直接使用 defineProps、defineEmits报错问题
+  },
+  globals: {
+    SysTyping: true,
+    UserTyping: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+    'plugin:prettier/recommended' // 添加prettier插件
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'vue/no-multiple-template-root': 'off', // 关闭对Vue文件唯一节点的检测
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'vue/no-v-model-argument': 'off',
+    'no-unused-vars': 'off',
+    'no-plusplus': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true, // 对开发依赖设置为true，不报错
+        optionalDependencies: false,
+        peerDependencies: false,
+        bundledDependencies: false
+      }
+    ],
+    'no-param-reassign': ['error', { props: false }]
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>VPlus Admin</div>
+</template>
+
+<script setup lang="ts"></script>
